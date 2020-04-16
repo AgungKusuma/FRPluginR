866ce6ec80ef0ec2c8bcbe0623915a4cb48f63f1
harvelouis <40568700+harvelouis@users.noreply.github.com>
2020-04-16 05:17:41 +0200

    Update ElementFaceWrapper.h

--- src/ios/ElementFaceWrapper.h
+++ src/ios/ElementFaceWrapper.h
@@ -4,6 +4,7 @@
 
 @interface ElementFaceWrapper : CDVPlugin
 
+- (void)getSdkVersion:(CDVInvokedUrlCommand *)command;
 - (void)enroll:(CDVInvokedUrlCommand *)command;
 - (void)match:(CDVInvokedUrlCommand *)command;
 
