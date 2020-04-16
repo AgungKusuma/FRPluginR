9dd1bfe078fabe2c93688e95281748d02b8a2aa6
harvelouis <40568700+harvelouis@users.noreply.github.com>
2020-04-16 05:20:03 +0200

    Update element.js

--- www/element.js
+++ www/element.js
@@ -2,6 +2,9 @@
 var exec = require('cordova/exec');
 
 var ElementSDK = {
+    getSdkVersion: function (id, successCallback, errorCallback) {
+        cordova.exec(successCallback, errorCallback, "ElementFaceMatchingSDK", "getSdkVersion");
+    },
     match: function (id, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "ElementFaceMatchingSDK", "match", [id]);
     },
