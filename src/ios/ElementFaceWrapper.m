79e884c0965302bb00b5ea0ff4c24beb324745b4
harvelouis <40568700+harvelouis@users.noreply.github.com>
2020-04-16 05:50:09 +0200

    Update ElementFaceWrapper.m

--- src/ios/ElementFaceWrapper.m
+++ src/ios/ElementFaceWrapper.m
@@ -11,7 +11,8 @@
 - (void)getSdkVersion:(CDVInvokedUrlCommand *)command {
     CDVPluginResult *pluginResult = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:[ElementSDKConfiguration shared].sdkVersion];
     [self.commandDelegate sendPluginResult:pluginResult callbackId:command.callbackId];
-    
+}
+  
 // private method
 - (BOOL)configureSDK {
     // TODO: update with your EAK
