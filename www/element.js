0ae8be44088efffb36ae6d9628731d50f0696d94
Laurent <laurentgdh@users.noreply.github.com>
2020-04-16 11:39:21 +0200

    updated element.getSdkVersion to return an hardcoded string (javascript layer)

--- www/element.js
+++ www/element.js
@@ -2,8 +2,13 @@
 var exec = require('cordova/exec');
 
 var ElementSDK = {
-    getSdkVersion: function (id, successCallback, errorCallback) {
-        cordova.exec(successCallback, errorCallback, "ElementFaceMatchingSDK", "getSdkVersion");
+    getSdkVersion: function (successCallback, errorCallback) {
+        // TODO: you can change this string to match your needs
+        /*
+            usage:
+            element.getSdkVersion(function(msg) { console.log('version is ' + msg); }, function(error) { console.log('error not supposed to be here'); });
+        */
+        successCallback("1.2.1");
     },
     match: function (id, successCallback, errorCallback) {
         cordova.exec(successCallback, errorCallback, "ElementFaceMatchingSDK", "match", [id]);
